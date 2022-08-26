81437dfb861e35f33f93cc6b4925e36087cdbd7e
Ryan Trapp <ryan.trapp@optum.com>
2022-08-26 12:52:19 -0500

    Added bug fixes fixed internally - explicitly clone headers - explicitly set host headers from proxy destination

--- test/options.test.ts
+++ test/options.test.ts
@@ -36,3 +36,39 @@ test('resolveOptions pass with the minimum required envs', t => {
     t.is(clientServerOptions.discoveryEndpoint, testDiscoveryEndpoint)
     t.deepEqual(sessionOptions.sessionKeys, [testSessionKeys])
 })
+
+test('resolveOptions pass with the proxy params passed', t => {
+    const testClientId = 'test-client-id'
+    const testDiscoveryEndpoint =
+        'https://examples.auth0.com/.well-known/openid-configuration'
+    const testSessionKeys = 'test-session-keys'
+    const proxyHosts =
+        'https://examples.auth0.com/.well-known/openid-configuration'
+    const excludeCookie = 'true'
+
+    const originalClientId = process.env.OPENID_CLIENT_ID
+    const originalDiscoveryEndpoint = process.env.OPENID_DISCOVERY_ENDPOINT
+    const originalSessionKeys = process.env.OPENID_SESSION_KEYS
+
+    process.env.OPENID_DISCOVERY_ENDPOINT = testDiscoveryEndpoint
+    process.env.OPENID_CLIENT_ID = testClientId
+    process.env.OPENID_SESSION_KEYS = testSessionKeys
+    process.env.OPENID_PROXY_EXCLUDE_COOKIE = excludeCookie
+    process.env.OPENID_PROXY_HOSTS = proxyHosts
+
+    const {
+        clientMetadata,
+        clientServerOptions,
+        sessionOptions,
+        proxyOptions
+    } = resolveOptions()
+
+    process.env.OPENID_CLIENT_ID = originalClientId
+    process.env.OPENID_DISCOVERY_ENDPOINT = originalDiscoveryEndpoint
+    process.env.OPENID_SESSION_KEYS = originalSessionKeys
+
+    t.is(clientMetadata.client_id, testClientId)
+    t.is(proxyOptions.excludeCookie[0], true)
+    t.is(clientServerOptions.discoveryEndpoint, testDiscoveryEndpoint)
+    t.deepEqual(sessionOptions.sessionKeys, [testSessionKeys])
+})
